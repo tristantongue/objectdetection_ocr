278fe474c0f75370b8d38a86d7ef76c93d3bde97
Tristan Tongue <tristantongue@gmail.com>
2020-11-18 11:40:28 -0600

    2nd commit

--- build.py
+++ build.py
@@ -1,6 +1,13 @@
 import glob
 import os
 import shutil
+from jinja2 import Template
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
+import face_recognition
 
 all_jpg_files = glob.glob("images/*.jpg")
 print(all_jpg_files)
@@ -22,17 +29,34 @@ for image in all_jpg_files:
             "output_filename": "%s.html" %name_only,
         }
     )
+#print(pages)
 
+def main():
+    for page in pages:
+        template_html = open("templates/base.html").read()
+        template = Template(template_html)  
+        page_output = page["output"]
+        tess_image = Image.open(page["image_path"])
+        detected_text = pytesseract.image_to_string(tess_image, lang="eng")
+        face_image = face_recognition.load_image_file(page["image_path"])
+        face_locations = face_recognition.face_locations(face_image)
+        image_height = tess_image.height
+        image_width = tess_image.width
+        print(image_height)
+        print(image_width)
+        print(face_locations)
+        print(detected_text)
+        open(page_output, "w+").write(template.render(
+            pages = pages,
+            title = page["title"],
+            image_source = page["image_path"],
+            detected_text = detected_text,
+            face_locations = face_locations,
+            image_height = image_height,
+            image_width = image_width,
+            ))
 
-def build_template(title):
-    template = open('templates/base.html').read()
-    ready_template = template.replace('{{title}}', title)
-    return ready_template
-
-print(pages)
-
-
-
+main()
 
 
 #    file_path = "images/first.jpg"
